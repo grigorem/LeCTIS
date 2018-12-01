e97406f577b8a3154d331b873d47d3f0d25b5586
Marius <Marius@DESKTOP-8G815BL>
2018-12-01 18:08:41 +0200

    The figure will be in the center of the screen at the start

--- LeCTIS.m
+++ LeCTIS.m
@@ -19,14 +19,21 @@ function varargout = LeCTIS(varargin)
 end
 
 
-function LeCTIS_OpeningFcn(hObject, eventdata, handles, varargin)
-    handles.output = hObject;
-    guidata(hObject, handles);
+function LeCTIS_OpeningFcn(hObject, eventdata, handles, varargin) 
+    % put the window in the middle of the screen
+    screenSize = get(groot, 'ScreenSize');
+    windowPosition = get(hObject, 'Position');
+    
+    newWindowPosition = windowPosition;
+    newWindowPosition(1) = floor(screenSize(3) / 2) - floor(windowPosition(3) / 2);
+    newWindowPosition(2) = floor(screenSize(4) / 2) - floor(windowPosition(4) / 2);
+    
+    set(hObject, 'Position', newWindowPosition);
 end
 
 
 function varargout = LeCTIS_OutputFcn(hObject, eventdata, handles) 
-    varargout{1} = handles.output;
+    varargout{1} = hObject;
 end
 
 %% UI controls callbacks
